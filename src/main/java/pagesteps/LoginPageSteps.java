41f974670d54b4e660b699b0b1a09a266265e3b4
Joshua Naval <joshua.naval@outlook.com>
2022-08-16 16:46:20 +0800

    Updated LoginPageSteps to Implement SeleniumActions Utility Class

--- src/main/java/pagesteps/LoginPageSteps.java
+++ src/main/java/pagesteps/LoginPageSteps.java
@@ -2,19 +2,20 @@ package pagesteps;
 
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
+import static utilities.actions.SeleniumActions.*;
 
 public class LoginPageSteps extends BasePageSteps {
     protected LoginPage loginPage;
 
     public LoginPageSteps(WebDriver webDriver) {
-        super.webDriver = webDriver;
+        super(webDriver);
         loginPage = new LoginPage(webDriver);
     }
 
     public void login() {
-        loginPage.txtEmail.sendKeys("user@phptravels.com");
-        loginPage.txtPassword.sendKeys("demouser");
-        loginPage.btnLogin.click();
+        type(waitForVisibilityOf(loginPage.txtEmail), "user@phptravels.com");
+        type(loginPage.txtPassword, "demouser");
+        click(waitToBeClickable(loginPage.btnLogin));
     }
 
 }
