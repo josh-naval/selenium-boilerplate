7238c60678859732509eaa398787e436284e5442
Joshua Naval <joshua.naval@outlook.com>
2022-08-16 16:45:45 +0800

    Added Constructor for the BasePageSteps Class
    Added a constructor for ease of setting a value for webdriver static variable and for the SeleniumActions webdriver and forcing the proper way of inheritting the abstract class

--- src/main/java/pagesteps/BasePageSteps.java
+++ src/main/java/pagesteps/BasePageSteps.java
@@ -1,6 +1,7 @@
 package pagesteps;
 
 import org.openqa.selenium.WebDriver;
+import utilities.actions.SeleniumActions;
 
 public abstract class BasePageSteps {
     protected static final String PAGE_URL = "https://www.phptravels.net/login";
@@ -10,6 +11,10 @@ public abstract class BasePageSteps {
     /*
      *  put methods that will be used by all page steps on this class
      */
+    public BasePageSteps(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        SeleniumActions.webDriver = this.webDriver;
+    }
 
     public void open_php_travels_page() {
         webDriver.get(PAGE_URL);
